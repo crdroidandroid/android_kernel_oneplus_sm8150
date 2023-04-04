681725898399173e24d48d17a19cf78a78a20ddf
Johan Hovold <johan+linaro@kernel.org>
2023-04-04 09:25:15 +0200

    USB: dwc3: fix runtime pm imbalance on unbind
    commit 44d257e9012ee8040e41d224d0e5bfb5ef5427ea upstream.
    
    Make sure to balance the runtime PM usage count on driver unbind by
    adding back the pm_runtime_allow() call that had been erroneously
    removed.
    
    Fixes: 266d0493900a ("usb: dwc3: core: don't trigger runtime pm when remove driver")
    Cc: stable@vger.kernel.org	# 5.9
    Cc: Li Jun <jun.li@nxp.com>
    Acked-by: Thinh Nguyen <Thinh.Nguyen@synopsys.com>
    Signed-off-by: Johan Hovold <johan+linaro@kernel.org>
    Link: https://lore.kernel.org/r/20230404072524.19014-3-johan+linaro@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Change-Id: I0d22fde65d458e35e3a1d4a2b4c4d4d01148a05f

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1518,8 +1518,9 @@ static int dwc3_remove(struct platform_device *pdev)
 	res->start -= DWC3_GLOBALS_REGS_START;
 
 	dwc3_gadget_exit(dwc);
-	pm_runtime_allow(&pdev->dev);
 	dwc3_debugfs_exit(dwc);
+
+	pm_runtime_allow(&pdev->dev);
 	pm_runtime_disable(&pdev->dev);
 	pm_runtime_put_noidle(&pdev->dev);
 	pm_runtime_set_suspended(&pdev->dev);
